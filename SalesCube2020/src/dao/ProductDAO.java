8bcb325061bf667657791a6e9808a9c480cd0fb2
cosmedia <cosmedia@LAPTOP-RDNU4F2S>
2020-06-16 18:00:55 +0900

    DAO追加

---
+++ SalesCube2020/src/dao/ProductDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.*;
+
+public class ProductDAO extends BaseDAO {
+
+}
