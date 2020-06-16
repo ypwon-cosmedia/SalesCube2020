f9e0c8e227bb82046a9c28b27251fd1f4efd2ecb
cosmedia <cosmedia@LAPTOP-RDNU4F2S>
2020-06-16 15:32:31 +0900

    123

--- SalesCube2020/src/beans/SetProductBean.java
+++ SalesCube2020/src/beans/SetProductBean.java
@@ -1,5 +1,37 @@
 package beans;
 
+/*
+ * セット商品に関した情報を保存するBeanクラスです。
+ */
+
 public class SetProductBean {
 	
+	/*商品コードを保存するメンバー変数*/
+	private String setProductCode;
+	
+	/*商品の中の商品コードを保存するメンバー変数」*/
+	private String productCode;
+	
+	/*商品の数量を保存するメンバー変数*/
+	private int quantity;
+	
+	
+	public String getSetProductCode() {
+		return setProductCode;
+	}
+	public void setSetProductCode(String setProductCode) {
+		this.setProductCode = setProductCode;
+	}
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 }
