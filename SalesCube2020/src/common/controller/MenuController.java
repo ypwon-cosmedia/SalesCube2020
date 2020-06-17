c686a11d5ddc2dc6d212e9b8a96381a9401dcadc
cosmedia <cosmedia@LAPTOP-RDNU4F2S>
2020-06-17 11:36:39 +0900

    MenuController追加

--- SalesCube2020/src/common/controller/MenuController.java
+++ SalesCube2020/src/common/controller/MenuController.java
@@ -10,13 +10,42 @@ import java.lang.*;
 import user.DAO.*;
 import user.beans.*;
 
-public class MenuController extends BaseController{
+final public class MenuController extends BaseController{
 	
 	protected MenuController() {
 	}
 	
 	public String execService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-      		return "";
-      	}
+			String forwardURL = "/menu.jsp";
+			String action = request.getParameter("action");
+			
+			if(action.equals("customer")) forwardURL = moveCustomer(request, response);
+			else if(action.equals("product")) forwardURL = moveProduct(request, response);
+			else if(action.equals("setProduct")) forwardURL = moveSetProduct(request, response);
+			else if(action.equals("menu")) forwardURL = moveMenu(request, response);
+			
+      		return forwardURL;
+      }
+	
+	private String moveCustomer (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		return "/customersearch.jsp";
+	}
+	
+	private String moveProduct (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		return "/productsearch.jsp";
+	}
+	
+	private String moveSetProduct (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		return "/setproduct.jsp";
+	}
+	
+	private String moveMenu (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		return "/menu.jsp";
+	}
+		
 }
