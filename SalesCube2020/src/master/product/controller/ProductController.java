9dd12b68992a0ab5867d0ae12f669b48f8879a3f
cosmedia <cosmedia@LAPTOP-PRF0U2GQ>
2020-06-18 17:56:29 +0900

    123

--- SalesCube2020/src/master/product/controller/ProductController.java
+++ SalesCube2020/src/master/product/controller/ProductController.java
@@ -28,7 +28,9 @@ public class ProductController extends BaseController{
 		else if(action.equals("moveModifyProduct")) forwardURL = moveModifyProduct(request, response);
 		else if(action.equals("productExcelInput")) forwardURL = productExcelInput(request, response);
 		else if(action.equals("productExcelOutput")) forwardURL = productExcelOutput(request, response);
+		else if(action.equals("productHistoryOutput")) forwardURL = productHistoryOutput(request, response);
 		else if(action.equals("initProduct")) forwardURL = initProduct(request, response);
+		else if(action.equals("updateInitProduct")) forwardURL = updateInitProduct(request, response);
 		else if(action.equals("quantityDiscount")) forwardURL = quantityDiscount(request, response);
 		else if(action.equals("searchProduct")) forwardURL = searchProduct(request, response);
 		else if(action.equals("deleteProduct")) forwardURL = deleteProduct(request, response);
@@ -67,22 +69,32 @@ public class ProductController extends BaseController{
 
 	private String quantityDiscount(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		return "/.jsp";
+		return "/productaddmodify.jsp";
 	}
 
 	private String initProduct(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		return "/productinit.jsp";
 	}
+	
+	private String updateInitProduct(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return "/productaddmodify.jsp";
+	}
 
 	private String productExcelOutput(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		return "/.jsp";
+		return "/productsearch.jsp";
 	}
 
 	private String productExcelInput(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		return "/.jsp";
+		return "/productsearch.jsp";
+	}
+	
+	private String productHistoryOutput(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return "/productaddmodify.jsp";
 	}
 
 	private String moveModifyProduct(HttpServletRequest request, HttpServletResponse response) {
