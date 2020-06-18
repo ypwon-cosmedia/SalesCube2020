7ae499742c6f914131a3cdc008aec7e3fe8fdde4
cosmedia <cosmedia@LAPTOP-0UBHCOJR>
2020-06-18 18:33:40 +0900

    UserDAO作成と、Beanの名前修正

--- SalesCube2020/src/user/DAO/UserDAO.java
+++ SalesCube2020/src/user/DAO/UserDAO.java
@@ -13,5 +13,76 @@ import common.dao.BaseDAO;
 import user.beans.*;
 
 public class UserDAO extends BaseDAO {
+	
+	public LoginBean login(String id, String password) {
+
+		try{
+			LoginBean user = null;
+
+			Connection con;
+			Statement stmt=null;
+			ResultSet rs=null;
+			String sql;
+
+			con=super.getConnection();
+			stmt=con.createStatement();
+			sql = "select * from user_mst_xxxxx where user_id = '"+ id +"' and password = '" + password + "'";
+
+			rs = stmt.executeQuery(sql);
+
+			if(null==rs)
+				return null;
+			else{
+				while(rs.next()){
+					user=new LoginBean();
+					user.setUserID(rs.getString("user_id"));
+					user.setPassword(rs.getString("password"));
+				}
+			}
+			return user;
+		} catch (ClassNotFoundException e){	
+			e.printStackTrace();
+			return null;
+		} catch (SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public UserInfoBean userInfo(String id) {
+		
+		try {
+			UserInfoBean userInfo = null;
+			
+			Connection con;
+			Statement stmt=null;
+			ResultSet rs=null;
+			String sql;
+			
+			con=super.getConnection();
+			stmt=con.createStatement();
+			sql = "select * from user_mst_xxxxx where user_id = '"+ id;
+			
+			rs = stmt.executeQuery(sql);
+
+			if(null==rs)
+				return null;
+			else{
+				while(rs.next()){
+					userInfo=new UserInfoBean();
+					userInfo.setUserID(rs.getString("user_id"));
+					userInfo.setNameKNJ(rs.getString("NameKNJ"));
+				}
+			}
+			return userInfo;
+			
+		}catch (ClassNotFoundException e){	
+			e.printStackTrace();
+			return null;
+		} catch (SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
