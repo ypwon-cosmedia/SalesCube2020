43a868cea03ca65df1ea97dbe9390163a87aff9f
cosmedia <cosmedia@LAPTOP-0UBHCOJR>
2020-06-19 16:23:28 +0900

    Excel出力、途中

--- SalesCube2020/src/master/product/controller/ProductExcelController.java
+++ SalesCube2020/src/master/product/controller/ProductExcelController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.*;
 import common.controller.BaseController;
 import master.product.DAO.ProductDAO;
 import master.product.beans.ProductCategoryBean;
+import master.product.beans.ProductModifyBean;
 
 import java.util.*;
 import java.sql.SQLException;
@@ -26,26 +27,62 @@ public class ProductExcelController extends BaseController{
 		String forwardURL = "/menu.jsp";
 		String action = request.getParameter("action");
 
+		try {
+			
 		if(action.equals("productExcelInput")) 			forwardURL = productExcelInput(request, response);
 		else if(action.equals("productExcelOutput")) 	forwardURL = productExcelOutput(request, response);
 		else if(action.equals("productHistoryOutput"))  forwardURL = productHistoryOutput(request, response);
 		
+		}catch (ServletException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
   		return forwardURL;
-      	}
+	}	
 	
 	private String productExcelOutput(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		List<ProductCategoryBean> list = new ArrayList<>();
+		List<ProductModifyBean> list = new ArrayList<>();
+		list = request.getAttribute("product");
 		
-		ProductDAO dao = new ProductDAO();
+		//DAOを呼ぶ
 		
-		//list = dao.getCategory();
-		
-		request.setAttribute("category", list);
+		try {
+			//検索結果のExcel出力
+			PrintWriter p = new PrintWriter( new BufferedWriter( new OutputStreamWriter( new FileOutputStream("//ファイルの場所//", false),"utf-8")));
+			
+			//ヘッダ
+			p.print();
+			p.print(",");
+			
+			
+			p.println();)
+			
+			//検索結果
+			for(int i = 0; i < .length; i++) {
+				p.print( getProductCode() );
+				p.print(",");
+				
+				
+				
+				
+				
+				p.println();
+				
+			}
+			//閉じる
+			p.close();
+			System.out.println("Excel出力完了");
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return "/productsearch.jsp";
 	}
 
+/**	
 	private String productExcelInput(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		List<ProductCategoryBean> list = new ArrayList<>();
@@ -61,6 +98,10 @@ public class ProductExcelController extends BaseController{
 	
 	private String productHistoryOutput(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		
+		
 		return "/productaddmodify.jsp";
 	}
+**/
+
 }
