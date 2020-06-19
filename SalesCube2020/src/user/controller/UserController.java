b31a199ed637c889f3c887d481f7df9bcf6e8a81
cosmedia <cosmedia@LAPTOP-0UBHCOJR>
2020-06-19 11:44:56 +0900

    UserController

--- SalesCube2020/src/user/controller/UserController.java
+++ SalesCube2020/src/user/controller/UserController.java
@@ -3,6 +3,7 @@ package user.controller;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.websocket.Session;
 
 import common.controller.BaseController;
 
@@ -23,15 +24,50 @@ public class UserController extends BaseController{
 		String forwardURL = "/login.jsp";
 		String action = request.getParameter("action");
 		
-		if(action.equals("login")) forwardURL = login(request, response);
-		else if(action.equals("logout")) forwardURL = logout(request, response);
+		try {
+		
+		if(action.equals("login")) 			forwardURL = login(request, response);
+		else if(action.equals("logout")) 	forwardURL = logout(request, response);
+		
+		
+		}catch (ServletException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 		
   		return forwardURL;
-    }
-
-	public String login(HttpServletRequest request, HttpServletResponse response) {
+  		
+	}
 		
-		return "/menu.jsp";
+	public String login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String forwardURL = "/login.jsp";
+		String message = null;
+		LoginBean user = new LoginBean();
+		UserDAO userdao = new UserDAO();
+		
+		String userID = request.getParameter("userId");
+		String password = request.getParameter("password");
+		
+		user = userdao.login(userID, password);
+		
+		if(user.getUserID() == null) {
+			message = "IDとパスワードが正しくありません";
+			request.setAttribute("loginError", message);
+			
+			forwardURL = "/login.jsp";
+		}else {
+			//セッション領域の取得
+			HttpSession session = request.getSession();
+			//セッションスコープに、IDと漢字名を登録
+			UserInfoBean userInfo = new UserInfoBean();
+			userInfo = userdao.userInfo(userID);
+			session.setAttribute("userInfo", userInfo);
+			
+			forwardURL = "/menu.jsp";
+		}
+		return forwardURL;
 	}
 	
 	public String logout(HttpServletRequest request, HttpServletResponse response) {
