be2eaad8de18f7ec23ca826de36a7c16ca3d1fbf
cosmedia <cosmedia@LAPTOP-0UBHCOJR>
2020-06-22 17:42:05 +0900

    exceloutput

--- SalesCube2020/src/master/product/controller/ProductExcelController.java
+++ SalesCube2020/src/master/product/controller/ProductExcelController.java
@@ -28,17 +28,13 @@ public class ProductExcelController extends BaseController{
 		String forwardURL = "/menu.jsp";
 		String action = request.getParameter("action");
 
-		try {
-			
-		if(action.equals("productExcelInput")) 			forwardURL = productExcelInput(request, response);
-		else if(action.equals("productExcelOutput")) 	forwardURL = productExcelOutput(request, response);
-		else if(action.equals("productHistoryOutput"))  forwardURL = productHistoryOutput(request, response);
+		if(action.equals("productExcelOutput")) 	forwardURL = productExcelOutput(request, response);
 		
-		}catch (ServletException e) {
-			e.printStackTrace();
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
+		/**本来のもの
+			if(action.equals("productExcelInput")) 			forwardURL = productExcelInput(request, response);
+			else if(action.equals("productExcelOutput")) 	forwardURL = productExcelOutput(request, response);
+			else if(action.equals("productHistoryOutput"))  forwardURL = productHistoryOutput(request, response);
+		 **/
   		return forwardURL;
 	}	
 	
@@ -46,21 +42,37 @@ public class ProductExcelController extends BaseController{
 		// TODO Auto-generated method stub
 		List<ProductModifyBean> list = new ArrayList<>();
 		
-		
+		try {
+			
 		//DAOを呼び出す
 		ProductSearchDAO psd = new ProductSearchDAO();
 		//検索条件の取得
 		ProductSearchBean bean = new ProductSearchBean();
-		request.setCharacterEncoding(utf-8);
-		bean.setProductCode( request.getParameter() );
 		
-		list = searchAllProduct( bean );
+		request.setCharacterEncoding("UTF-8");
+		bean.setProductCode( request.getParameter("productCode") );
+		bean.setProductName( request.getParameter("productName") );
+		bean.setProductKana( request.getParameter("productKana") );
+		bean.setSupplierCode( request.getParameter("supplierCode") );
+		bean.setSupplierName( request.getParameter("supplierName") );
+		bean.setProductStandardCategory( request.getParameter("productStandardCategory") );
+		bean.setProductStatusCategory( request.getParameter("productStatusCategory") );
+		bean.setProductStockCategory( request.getParameter("productStockCategory") );
+		bean.setSetTypeCategory(  Integer.parseInt( request.getParameter("setTypeCategory") )  );
+		bean.setRemarks( request.getParameter("remarks") );
+		bean.setProduct1( request.getParameter("product1") );
+		bean.setProduct2( request.getParameter("product2") );
+		bean.setProduct3( request.getParameter("product3") );
 		
-		
-		try {
-			
+		//検索結果をlistに入れる
+			try {
+				list = psd.searchAllProduct( bean );
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 			//CSVファイルの指定
-			PrintWriter p = new PrintWriter( new BufferedWriter( new OutputStreamWriter( new FileOutputStream("//Excelファイルの場所//", false),"utf-8")));
+			PrintWriter p = new PrintWriter( new BufferedWriter( new OutputStreamWriter( new FileOutputStream("C:\\Users\\cosmedia\\Desktop\\SalesCubeテスト用/商品マスタ.csv", false),"utf-8")));
 			
 			//ヘッダ部
 			p.print("商品コード");
@@ -212,151 +224,162 @@ public class ProductExcelController extends BaseController{
 				
 				p.print( list.get(i).getProductCode() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductName() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductKana() );
 	            p.print(",");
-				p.print();
+				p.print( list.get(i).getOnlinePcode() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSupplierPcode() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSupplierCode());
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getRackCode() );
+	            p.print(",");
+				p.print( list.get(i).getSupplierPriceYen() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSupplierPriceDol() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getRetailPrice() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSoRate() );
+	            p.print(",");
+				p.print( list.get(i).getUnitCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getPackQuantity() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getJanPcode() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getWidth() );
+	            p.print(",");
+	            p.print( list.get(i).getWidthUnitSizeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDepth() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDepthUnitSizeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getHeight() );
+	            p.print(",");
+	            p.print( list.get(i).getHeightUnitSizeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getWeight() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getWeightUnitSizeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getLength() );
+	            p.print(",");
+	            p.print( list.get(i).getLengthUnitSizeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getPoLot() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getLotUpdFlag() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getLeadTime() );
+	            p.print(",");
+	            p.print( list.get(i).getPoNum() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getPoUpdFlag() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getAvgShipCount() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getMaxStockNum() );
+	            p.print(",");
+	            p.print( list.get(i).getStockUpdFlag() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getTermShipNum() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getMaxPoNum() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getMaxPoUpdFlag() );
+	            p.print(",");
+	            p.print( list.get(i).getFractCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getTaxCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getStockCtlCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getStockAssesCategory() );
+	            p.print(",");
+	            p.print( list.get(i).getProductCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProduct1() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProduct2() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProduct3() );
+	            p.print(",");
+	            p.print( list.get(i).getRoMaxNum() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductRank() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSetTypeCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductStatusCategory() );
+	            p.print(",");
+	            p.print( list.get(i).getProductStockCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductPurvayCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getProductStandardCategory() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getCoreNum() );
+	            p.print(",");
+	            p.print( list.get(i).getNum1() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getNum2() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getNum3() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getNum4() );
+	            p.print(",");
+	            p.print( list.get(i).getNum5() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDec1() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDec2() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDec3() );
+	            p.print(",");
+	            p.print( list.get(i).getDec4() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDec5() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDiscardDate() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getRemarks() );
+	            p.print(",");
+	            p.print( list.get(i).getEadRemarks() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getCommentData() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getLastRoDate() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getSalesStandardDeviation() );
+	            p.print(",");
+	            p.print( list.get(i).getMineSafetyStock() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getMineSafetyStockUpdFlag() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getEntrustSafetyStock() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getCreFunc() );
+	            p.print(",");
+	            p.print( list.get(i).getCreDatetm() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getCreUser() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getUpdFunc() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getUpdDatetm() );
+	            p.print(",");
+	            p.print( list.get(i).getUpdUser() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDelFunc() );
 				p.print(",");
-				p.print();
+				p.print( list.get(i).getDelDatetm() );
 				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				p.print();
-				p.print(",");
-				
+				p.print( list.get(i).getDelUser() );
+	            
 				//改行
 				p.println();
 				
@@ -367,10 +390,9 @@ public class ProductExcelController extends BaseController{
 			
 		}catch (IOException e) {
 			e.printStackTrace();
-		}catch (ServletException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
 		return "/productsearch.jsp";
 	}
 
