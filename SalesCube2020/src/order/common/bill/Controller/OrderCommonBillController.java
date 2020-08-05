7ccfee5119b0d70218ab9de3b6390f359db9ea71
cosmedia <cosmedia@192.168.2.35>
2020-08-05 17:26:51 +0900

    huyji

--- SalesCube2020/src/order/common/bill/Controller/OrderCommonBillController.java
+++ SalesCube2020/src/order/common/bill/Controller/OrderCommonBillController.java
@@ -2,12 +2,16 @@ package order.common.bill.Controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.BaseController;
+import order.common.bill.DAO.OrderCommonBillDAO;
+import order.common.bill.beans.OrderCommonBillBean;
 
 public class OrderCommonBillController extends BaseController {
 
@@ -28,8 +32,22 @@ public class OrderCommonBillController extends BaseController {
 		return forwardURL;
 	}
 	
+	/* 見積検索 */
+	private String searchEstimate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, ClassNotFoundException {
+		
+		List<OrderCommonBillBean> list1 = new ArrayList<>();
+		OrderCommonBillDAO dao1 = new OrderCommonBillDAO();
+		
+		return null;
+		
+	}
+	
+	/* 見積番号押下 受注新規登録画面に反映 */
 	private String EstimateToOrderInput(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, ClassNotFoundException {
 		
+		OrderCommonBillDAO dao = new OrderCommonBillDAO();
+		OrderCommonBillBean bean1 = new OrderCommonBillBean();
+				
 		
 		return null;
 		
--- SalesCube2020/src/order/input/controller/OrderInputController.java
+++ SalesCube2020/src/order/input/controller/OrderInputController.java
@@ -1,6 +1,7 @@
 package order.input.controller;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -27,6 +28,8 @@ public class OrderInputController extends BaseController {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+		}else if(action.equals("orderinputCmp")) {
+			forwardURL = orderInputCmp(request, response);
 		}else if(action.equals("orderupdate")) {
 				forwardURL = orderUpdate(request, response);
 		}
@@ -51,11 +54,82 @@ public class OrderInputController extends BaseController {
 		
 	}
 	
+	/* 受注新規登録 */
+	private String orderInputCmp(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException, ClassNotFoundException {
+		
+		OrderInputDAO dao = new OrderInputDAO();
+		
+		Date roDate = request.getParameter("roDate");
+		Date shipDate = nullOrEmpty(request.getParameter("shipDate"));
+		Date deliveryDate = nullOrEmpty(request.getParameter("deliveryDate"));
+		String ReceptNo = nullOrEmpty(request.getParameter("ReceptNo"));
+		String customerSlipNo;
+		String userName;
+		String remarks;
+		String dcName;
+		String dcTimezone;
+		String ctaxRate;
+		String customerCode;
+		String customerName;
+		String taxShiftCategory;
+		String cutoffGroup;
+		String salesCmCategory;
+		String customerRemarks;
+		String customerCommentData;
+		String deliveryName;
+		String deliveryOfficeName;
+		String deliveryDeptName;
+		String deliveryZipCode;
+		String deliveryAddress1;
+		String deliveryAddress2;
+		String deliveryPcName;
+		String deliveryPcKana;
+		String deliveryPcPre;
+		String deliveryTel;
+		String deliveryFax;
+		String deliveryEmail;
+		String productCode;
+		String productName;
+		String productRemarks;
+		String rackCode;
+		Integer quantity;
+		Integer unitCost;
+		Integer cost;
+		Integer unitRetailPrice;
+		Integer retailPrice;
+		String inputProductRemarks;
+		String eadRemarks;
+		String prossProfit;
+		String grossProfitRatio;
+		String retailPriceTotal;
+		Integer ctaxPriceTotal;
+		String priceTotal;
+		
+		OrderInputBean bean = new OrderInputBean();
+		bean.setRoDate(roDate);
+		bean.setShipDate(shipDate);
+		bean.setDeliveryDate(deliveryDate);
+		bean.setReceptNo(ReceptNo);
+		bean.setCustomerSlipNo(customerSlipNo);
+		
+		
+		return "order\\orderinput.jsp";
+		
+	}
+	
+
 	/* 受注更新 */
 	private String orderUpdate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		return null;
 		
+	}
+	
+	public String nullOrEmpty(String str) {
+		if (str == null || str.equals("")) {
+			str = null;
+		}
+		return str;
 	}
 	
 }
