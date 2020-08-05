3273bf2685a14dce13d94aad79833a942bab9dc8
cosmedia <cosmedia@192.168.2.43>
2020-08-05 11:26:38 +0900

    123

--- SalesCube2020/src/estimate/common/controller/UserModalAJAXController.java
+++ SalesCube2020/src/estimate/common/controller/UserModalAJAXController.java
@@ -1,5 +1,20 @@
 package estimate.common.controller;
 
-public class UserModalAJAXController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.BaseAJAXController;
+
+public class UserModalAJAXController extends BaseAJAXController {
+
+	@Override
+	public void execService(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
