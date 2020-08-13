629dd5b732ec1732f8db35a80cb3cb9174879d48
cosmedia <cosmedia@192.168.2.43>
2020-08-13 17:20:15 +0900

    123

---
+++ SalesCube2020/src/estimate/Search/controller/EstimateSearchAJAXController.java
@@ -0,0 +1,15 @@
+package estimate.Search.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.BaseAJAXController;
+
+public class EstimateSearchAJAXController extends BaseAJAXController{
+	public void execService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
