56d8b5601c4371cce0c5514478ba68bd79cf7e84
cosmedia <cosmedia@192.168.2.34>
2020-08-25 10:08:54 +0900

    a

--- SalesCube2020/src/estimate/Search/controller/EstimateSearchAJAXController.java
+++ SalesCube2020/src/estimate/Search/controller/EstimateSearchAJAXController.java
@@ -1,6 +1,11 @@
 package estimate.Search.controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +25,7 @@ import estimate.Search.beans.EstimateSearchBean;
 import estimate.Search.beans.EstimateSearchResultBean;
 import estimate.Search.dao.EstimateSearchDAO;
 import estimate.common.beans.EstimateConfigurationBean;
+import master.product.beans.ProductModifyBean;
 import user.beans.UserInfoBean;
 
 
@@ -33,10 +39,98 @@ public class EstimateSearchAJAXController extends BaseAJAXController{
 		try {
 			if(action.equals("estimateSearch")) estimateSearch(request,response);
 			else if(action.equals("estimateCreateTable")) estimateCreateTable(request,response);
-		} catch (MissingResourceException | ServletException | IOException e) {
+			else if(action.equals("estimateExcelOutput")) estimateExcelOutput(request,response);
+		} catch (MissingResourceException | ServletException | IOException | ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void estimateExcelOutput(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException, SQLException {
+		EstimateSearchDAO dao = new EstimateSearchDAO();
+		EstimateSearchBean bean = new EstimateSearchBean();
+		UserInfoBean user = new UserInfoBean();
+		
+		List<String[]> list = new ArrayList<>();
+		
+		try {
+			//ユーザー情報取得
+			HttpSession session = request.getSession();
+			user =(UserInfoBean)session.getAttribute("userInfo");
+			 
+			//見積検索条件取得
+			bean.setEstimateSheetId(request.getParameter("estimateSheetId"));
+			bean.setEstimateDateStart(request.getParameter("estimateDateStart"));
+			bean.setEstimateDateEnd(request.getParameter("estimateDateEnd"));
+			bean.setValidDateStart(request.getParameter("validDateStart"));
+			bean.setValidDateEnd(request.getParameter("validDateEnd"));
+			bean.setUserId(request.getParameter("userId"));
+			bean.setUserName(request.getParameter("userName"));
+			bean.setTitle(request.getParameter("title"));
+			bean.setRemarks(request.getParameter("remarks"));
+			bean.setSubmitName(request.getParameter("submitName"));
+			bean.setCustomerCode(request.getParameter("customerCode"));
+			bean.setCustomerName(request.getParameter("customerName"));
+			
+			//ソート条件取得(見積番号順で固定)
+			String sort =  "estimateSheetID";
+			
+			//検索結果をlistに入れる
+			try {
+				
+				list = dao.estimateSearchResult(bean, user, sort);
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			// 出力ファイルの作成
+	        File file = new File("C:/Users/cosmedia/Desktop/見積検索結果.csv");
+	
+	        int k = 1;
+	        while( file.exists() ){
+	        	file = new File("C:/Users/cosmedia/Desktop/見積検索結果("+ k +").csv");
+	        		k++;
+	        }
+	        
+	        PrintWriter p = new PrintWriter( new BufferedWriter( new OutputStreamWriter( new FileOutputStream(file, false),"SJIS")));
+
+			
+	        List<EstimateConfigurationBean> list2 = dao.estimateConfiguration(user); //ヘッダ部の並びを取得
+	        
+			//ヘッダ部
+	        for(EstimateConfigurationBean config : list2) {
+	    		p.print(config.getItemName());
+	    		p.print(",");
+	        }
+	
+			p.println();//改行
+	
+			//ボディ部
+			for(int i = 0; i < list.size(); i++) {
+				String[] raw = list.get(i);;
+				
+				for(int j = 0; j < raw.length; j++) {
+					p.print(raw[j]);
+					p.print(",");
+				}
+			}
+			
+			p.close();
+			
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("IOException");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("ClassNotFoundException");
+		}
+		System.out.println("終了処理");
+		
+		
+	}
+	
+	
 
 	
 	public void estimateSearch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
